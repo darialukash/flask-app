721d5840978e13c233300182a153639f300917ca
Daria <daria.a.lukash@gmail.com>
2019-09-10 15:32:36 +0300

    app create DB

--- simpleapp/__init__.py
+++ simpleapp/__init__.py
@@ -12,7 +12,7 @@ def create_app():
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY') or 'dev_key',
         SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or \
-                                'sqlite:///' + os.path.join(app.instance_path, 'person_list.sqlite'),
+                                'postgresql://postgres:postgres@localhost/flaskdb',
         SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
 
