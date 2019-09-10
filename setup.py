cd7bce56a1e2aadca8a1d05f0e3fac62f1f7a794
Daria <daria.a.lukash@gmail.com>
2019-09-10 22:01:44 +0300

    app create DB

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name='simpleapp',
     packages=['simpleapp'],
-    include_pakage_data=True,
-    install_requests=['flask']
+    #include_pakage_data=True,
+    #install_requests=['flask']
 )
