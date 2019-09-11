79bac6eb1456fb0dd11d489dfe1647e513787cb4
Daria <daria.a.lukash@gmail.com>
2019-09-11 20:25:46 +0300

    procfile

--- tests/test_app.py
+++ tests/test_app.py
@@ -16,7 +16,7 @@ def app():
 def test_test_conf(app):
     app.config.from_object(ConfTest)
     assert app.config['DEBUG']
-    assert app.config['SQLALCHEMY_DATABASE_URI'] == os.environ.get('DATABASE_TEST_URL')
+    assert app.config['SQLALCHEMY_DATABASE_URI']
     assert app.config['TESTING']
     assert not app.config['PRESERVE_CONTEXT_ON_EXCEPTION']
 
